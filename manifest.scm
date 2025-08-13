99984d29abcb44bf5c5d2b77bc457dbc5d0b82f4
FuncProgLinux <softwarelibre@urutau-ltd.org>
2025-08-12 18:19:26 -0600

    fuck services as well

--- manifest.scm
+++ manifest.scm
@@ -1,12 +1,13 @@
-(concatenate-manifests (list (specifications->manifest (list "git"
-                                                        "emacs"
+;; Lo que sigue es un "manifest" equivalente a la línea de comando que
+;; introdujo. Puede almacenarlo dentro de un archivo que pudiese pasar a
+;; cualquier comando 'guix' que acepte una opción '--manifest' (o -m).
+
+(concatenate-manifests (list (specifications->manifest (list "emacs"
                                                         "emacs-geiser"
                                                         "emacs-geiser-guile"
-                                                        "emacs-arei"
                                                         "emacs-paredit"
+                                                        "guile-lsp-server"
                                                         "make"
-                                                        "perl"
-                                                        "emacs-evil"))
+                                                        "perl"))
                              (package->development-manifest (specification->package
-                                                             "git" "guile"
-                                                             "guile-colorized"))))
+                                                             "git"))))
