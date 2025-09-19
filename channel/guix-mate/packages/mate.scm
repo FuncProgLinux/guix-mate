75a3ec8f8d8d9782e29af35c0949b77fab6a281d
FuncProgLinux <softwarelibre@urutau-ltd.org>
2025-09-19 12:16:31 -0600

    upsteam mate-sensors-applet

--- channel/guix-mate/packages/mate.scm
+++ channel/guix-mate/packages/mate.scm
@@ -58,61 +58,6 @@
   #:use-module (guix-mate packages mate-tweak)
   #:use-module (guix-mate packages mate-window-applets))
 
-(define-public mate-sensors-applet
-  (package
-    (name "mate-sensors-applet")
-    (version "1.28.0")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (string-append "mirror://mate/"
-                           (version-major+minor version)
-                           "/"
-                           "mate-sensors-applet-"
-                           version
-                           ".tar.xz"))
-       (sha256
-        (base32 "10as64102wbgmi8ak3ya2zyvc3dpx24rfg18323fp3xgh9k3crfl"))))
-    (build-system glib-or-gtk-build-system)
-    (arguments
-     (list
-      #:configure-flags
-      #~(list "--enable-in-process"
-              (string-append "--libexecdir="
-                             #$output "/libexec"))))
-    (native-inputs (list pkg-config
-                         intltool ;Listed in Debian package (but not in upstream build.yml)
-                         itstool ;Listed in upstream build.yml
-                         yelp-tools
-                         gettext-minimal
-                         gobject-introspection))
-    (inputs (list at-spi2-core
-                  dbus
-                  dbus-glib
-                  glib
-                  gtk+
-                  libnotify
-                  lm-sensors
-                  libx11
-                  libxml2
-                  libxslt
-                  libatasmart
-                  libwnck
-                  mate-desktop
-                  mate-menus
-                  mate-panel
-                  pango
-                  polkit ;either polkit or setuid
-                  upower
-                  wireless-tools))
-    (home-page "https://mate-desktop.org/")
-    (synopsis "MATE panel applet for hardware sensors.")
-    (description
-     "MATE Sensors Applet displays readings from hardware sensors in the MATE
-panel, these include CPU temperature, fan speeds and voltage reading under
-Gnu/Linux.")
-    (license license:gpl2+)))
-
 (define-public engrampa-1.28.2
   (package
     (name "engrampa")
