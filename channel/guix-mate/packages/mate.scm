35f0e0fb43353c6fb769a2e859b27fbbf493a075
FuncProgLinux <softwarelibre@urutau-ltd.org>
2025-10-20 17:12:51 -0600

    guix-mate: Add mate-panel-1.28.7
    * channel/guix-mate/packages/mate.scm (mate-panel-1.28.7): New variable.

--- channel/guix-mate/packages/mate.scm
+++ channel/guix-mate/packages/mate.scm
@@ -5,6 +5,7 @@
   #:use-module (guix build-system gnu)
   #:use-module (guix build-system trivial)
   #:use-module (guix download)
+  #:use-module (guix git-download)
   #:use-module (guix gexp)
   #:use-module (guix packages)
   #:use-module (guix utils)
